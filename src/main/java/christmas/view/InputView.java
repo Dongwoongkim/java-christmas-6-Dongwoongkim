3b94392d850209260b2c54ce79f77126090f1e35
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-10 03:38:39 +0900

    feat : 기본 입출력 클래스 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,18 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+    public String inputDay() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String day = Console.readLine();
+        return day;
+    }
+
+    public String inputMenu() {
+        System.out.println("주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String menu = Console.readLine();
+        return menu;
+    }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,4 +2,31 @@ package christmas.view;
 
 public class OutputView {
 
+    public void printMenu() {
+        System.out.println("<주문 메뉴>");
+    }
+
+    public void printBeforeDiscount() {
+        System.out.println("<할인 전 총주문 금액>");
+    }
+
+    public void printServiceMenu() {
+        System.out.println("<증정 메뉴>");
+    }
+
+    public void printBenefit() {
+        System.out.println("<혜택 내역>");
+    }
+
+    public void printTotalBenefit() {
+        System.out.println("<총혜택 금액>");
+    }
+
+    public void printPayMoneyAfterDiscount() {
+        System.out.println("<할인 후 예상 결제 금액>");
+    }
+
+    public void printBadge() {
+        System.out.println("<12월 이벤트 배지>");
+    }
 }
