0536c23e6a58ee2624d28e7927e28ee38ea2857e
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-12 16:16:33 +0900

    fix : 잘못된 에러메시지 수정

--- src/main/java/christmas/exception/DayDoesNotExistInCalendarException.java
+++ src/main/java/christmas/exception/DayDoesNotExistInCalendarException.java
@@ -1,10 +1,10 @@
 package christmas.exception;
 
-import static christmas.exception.message.ErrorMessage.INVALID_INPUT_MENU_ERROR_MESSAGE;
+import static christmas.exception.message.ErrorMessage.INVALID_INPUT_DAY_ERROR_MESSAGE;
 
 public class DayDoesNotExistInCalendarException extends IllegalArgumentException {
 
     public DayDoesNotExistInCalendarException() {
-        super(INVALID_INPUT_MENU_ERROR_MESSAGE.getMessage());
+        super(INVALID_INPUT_DAY_ERROR_MESSAGE.getMessage());
     }
 }
