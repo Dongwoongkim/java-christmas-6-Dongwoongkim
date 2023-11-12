2f2e08a23fbcc17c544e841479b4d9cf8e0a3f29
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-13 03:38:43 +0900

    refactor : 연관성이 있는 상수 enum으로 분리

--- src/main/java/christmas/model/Badge.java
+++ src/main/java/christmas/model/Badge.java
@@ -1,15 +1,11 @@
 package christmas.model;
 
-import static christmas.model.EventInfo.SANTA_BADGE_REQUIRE_AMOUNT;
-import static christmas.model.EventInfo.STAR_BADGE_REQUIRE_AMOUNT;
-import static christmas.model.EventInfo.TREE_BADGE_REQUIRE_AMOUNT;
+import static christmas.model.BadgeInfo.NONE_BADGE;
+import static christmas.model.BadgeInfo.SANTA_BADGE;
+import static christmas.model.BadgeInfo.STAR_BADGE;
+import static christmas.model.BadgeInfo.TREE_BADGE;
 
 public class Badge {
-
-    private static final String SANTA_BADGE = "산타";
-    private static final String TREE_BADGE = "트리";
-    private static final String STAR_BADGE = "별";
-    private static final String NONE_BADGE = "없음";
 
     private final String name;
 
@@ -18,16 +14,16 @@ public class Badge {
     }
 
     public static Badge create(final Integer sumOfDiscount) {
-        if (sumOfDiscount >= SANTA_BADGE_REQUIRE_AMOUNT.getValue()) {
-            return new Badge(SANTA_BADGE);
+        if (sumOfDiscount >= SANTA_BADGE.getRequireAmount()) {
+            return new Badge(SANTA_BADGE.getBadgeName());
         }
-        if (sumOfDiscount >= TREE_BADGE_REQUIRE_AMOUNT.getValue()) {
-            return new Badge(TREE_BADGE);
+        if (sumOfDiscount >= TREE_BADGE.getRequireAmount()) {
+            return new Badge(TREE_BADGE.getBadgeName());
         }
-        if (sumOfDiscount >= STAR_BADGE_REQUIRE_AMOUNT.getValue()) {
-            return new Badge(STAR_BADGE);
+        if (sumOfDiscount >= STAR_BADGE.getRequireAmount()) {
+            return new Badge(STAR_BADGE.getBadgeName());
         }
-        return new Badge(NONE_BADGE);
+        return new Badge(NONE_BADGE.getBadgeName());
     }
 
     public String getName() {
---
+++ src/main/java/christmas/model/BadgeInfo.java
@@ -0,0 +1,25 @@
+package christmas.model;
+
+public enum BadgeInfo {
+
+    SANTA_BADGE("산타", 20000),
+    TREE_BADGE("트리", 10000),
+    STAR_BADGE("별", 5000),
+    NONE_BADGE("없음", 0);
+
+    private final String badgeName;
+    private final Integer requireAmount;
+
+    BadgeInfo(String badgeName, Integer requireAmount) {
+        this.badgeName = badgeName;
+        this.requireAmount = requireAmount;
+    }
+
+    public String getBadgeName() {
+        return badgeName;
+    }
+
+    public Integer getRequireAmount() {
+        return requireAmount;
+    }
+}
