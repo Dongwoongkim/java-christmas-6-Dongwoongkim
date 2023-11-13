83ebb2979513ecc45ad6fa7312aa4e36b5b42651
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-13 20:49:10 +0900

    test(add) : 방문 날짜에 대한 값 검사 메소드 테스트 추가

--- src/test/java/christmas/model/vo/VisitDayTest.java
+++ src/test/java/christmas/model/vo/VisitDayTest.java
@@ -2,9 +2,15 @@ package christmas.model.vo;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import christmas.exception.DayDoesNotExistInCalendarException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class VisitDayTest {
 
@@ -17,5 +23,76 @@ class VisitDayTest {
         // then
         VisitDay visitDay = assertDoesNotThrow(() -> new VisitDay(validDay));
         assertEquals(validDay, visitDay.day());
+    }
+
+    @DisplayName("1-31사이의 숫자가 아닌 경우 예외가 발생하며 객체가 생성되지 않는다.")
+    @Test
+    void invalidDay_OutMonth_test_() {
+        // then
+        assertThrows(DayDoesNotExistInCalendarException.class, () -> new VisitDay(40));
+    }
+
+    @DisplayName("필드값이 주말에 속하는 경우 true를 리턴한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 8, 9, 15, 16, 22, 23, 29, 30})
+    void isWeekend_true_test(Integer day) {
+        // when
+        VisitDay visitDay = new VisitDay(day);
+
+        // then
+        assertTrue(visitDay.isWeekend());
+    }
+
+    @DisplayName("필드값이 평일에 속하는 경우 false를 리턴한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5, 6, 7, 10, 11, 12, 13, 14})
+    void isWeekend_false_test(Integer day) {
+        // when
+        VisitDay visitDay = new VisitDay(day);
+
+        // then
+        assertFalse(visitDay.isWeekend());
+    }
+
+    @DisplayName("크리스마스 이전인 경우 true를 리턴한다.")
+    @Test
+    void isBeforeOrEqualsChristmas_test() {
+        // when
+        VisitDay visitDay = new VisitDay(20);
+
+        // then
+        assertTrue(visitDay.isBeforeOrEqualsChristmas());
+    }
+
+    @DisplayName("크리스마스 이후인 경우 false를 리턴한다.")
+    @Test
+    void isNotBeforeOrEqualsChristmas_test() {
+        // when
+        VisitDay visitDay = new VisitDay(26);
+
+        // then
+        assertFalse(visitDay.isBeforeOrEqualsChristmas());
+    }
+
+    @DisplayName("스페셜 데이인 경우 true를 리턴한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 10, 17, 24, 25, 31})
+    void isSpecialDay_test(Integer day) {
+        // when
+        VisitDay visitDay = new VisitDay(day);
+
+        // then
+        assertTrue(visitDay.isSpecialDay());
+    }
+
+    @DisplayName("스페셜 데이가 아닌경우 false를 리턴한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7})
+    void isNotSpecialDay_test(Integer day) {
+        // when
+        VisitDay visitDay = new VisitDay(day);
+
+        // then
+        assertFalse(visitDay.isSpecialDay());
     }
 }
